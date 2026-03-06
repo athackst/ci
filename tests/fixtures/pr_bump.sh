fbedba568ad53100a1ee36bf15107d450084ba7f
Allison Thackston <allison@allisonthackston.com>
2026-03-05 22:46:02 -0800

    Update env RESOLVED_VERSION to VERSION (#81)

--- tests/fixtures/pr_bump.sh
+++ tests/fixtures/pr_bump.sh
@@ -9,11 +9,10 @@ cat > "$OUT_FILE" <<EOF
 run_id=${GITHUB_RUN_ID:-}
 run_attempt=${GITHUB_RUN_ATTEMPT:-}
 from_ref=${FROM_REF:-}
-resolved_version=${RESOLVED_VERSION:-}
+version=${VERSION:-}
 major_version=${MAJOR_VERSION:-}
 minor_version=${MINOR_VERSION:-}
 patch_version=${PATCH_VERSION:-}
 version_parts=${MAJOR_VERSION:-}.${MINOR_VERSION:-}.${PATCH_VERSION:-}
 pr_info_path=${PR_INFO_PATH:-}
 EOF
-
