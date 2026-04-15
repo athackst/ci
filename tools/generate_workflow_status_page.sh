4b6717d9f8238beae32d09515148b84bb72b73e8
Allison Thackston <6098197+athackst@users.noreply.github.com>
2026-04-15 08:59:18 -0700

    Batch workflow status checks with GraphQL (#192)

--- tools/generate_workflow_status_page.sh
+++ tools/generate_workflow_status_page.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+export GH_TOKEN="$(gh auth token)"
+gh api graphql -f query='{ rateLimit { limit remaining resetAt cost } }'
+
 python3 actions/list-repos/list_repos.py \
   --debug-timing \
   --user athackst \
   --fork false \
   --archived false \
+  --private false \
   --filter-path .copier-answers.ci.yml > /tmp/repositories.json
 
-python3 .github/scripts/generate_workflow_status.py --repos-file /tmp/repositories.json --visibility public
+python3 .github/scripts/generate_workflow_status.py --repos-file /tmp/repositories.json
+gh api graphql -f query='{ rateLimit { limit remaining resetAt cost } }'
